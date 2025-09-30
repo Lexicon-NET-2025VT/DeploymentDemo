ebf471c8dbe0026b1c16005b3828126a8e454c95
Jonathan Krall <jonathan.krall@lexicon.se>
2025-09-30 10:56:52 +0200

    Add project files.

---
+++ Lexicon2020VTDeployment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lexicon2020VTDeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
